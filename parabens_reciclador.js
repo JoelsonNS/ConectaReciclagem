558449f97954f1ade1d363ab0bd3feeca1b4b35b
joelsonNS <joelson.n.s@hotmail.com>
2025-10-10 15:04:21 -0300

    melhorias de detalhes
    melhorias na páginas

--- parabens_reciclador.js
+++ parabens_reciclador.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const frase_reciclador = document.querySelector(".frase_parabens_reciclador");
     const nome_recicla = localStorage.getItem("Nome_Reciclador");
-    frase_reciclador.innerText = `Olá, ${nome_recicla}! Parabéns pela iniciativa de contribuir para um mundo mais limpo!`;
+    frase_reciclador.innerText = `Olá, Parabéns pela iniciativa de contribuir para um mundo mais limpo!`;
 
      const botao_reciclador = document.getElementById('botao_inicio_reciclador');
     botao_reciclador.addEventListener('click', function() {
